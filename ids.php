f4e5133568e415b41011fb9847d702c277f237f6
Shoichi Chou <shoichi.chou@gmail.com>
2016-06-28 04:17:40 +0800

    correct the path

--- ids.php
+++ ids.php
@@ -1,3 +1,3 @@
 <?php
-$wgAutoloadClasses['IDS'] = $IP . '/extensions/IDS/ids_body.php';
+$wgAutoloadClasses['IDS'] = $IP . '/extensions/Mediawiki-IDSextension/ids_body.php';
 $wgHooks['ParserFirstCallInit'][] = 'IDS::onParserInit';
